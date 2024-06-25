6a597c209c7a08a3a7c00d71415a01827f5388ea
BielC19 <118078513+BielC19@users.noreply.github.com>
2024-06-25 23:21:04 +0200

    app creada

---
+++ web/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Pgpri from "./pgpri";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Pgpri />
+  </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ web/src/pgpri.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pgpri.css';
+
+function Pgpri() {
+    return (
+    <header>
+        <h1>Can Sici</h1>
+    </header>
+    );
+}
+
+export default Pgpri;
